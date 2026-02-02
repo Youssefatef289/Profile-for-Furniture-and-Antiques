80f5c6eb4741623eec0b3b433a399e46fb7d81f9
Youssef Atef <youssefatef289@gmail.com>
2026-02-02 15:37:20 +0200

    Fix: Remove unused FaEnvelope import from Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import { FaPhone, FaMapMarkerAlt } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
